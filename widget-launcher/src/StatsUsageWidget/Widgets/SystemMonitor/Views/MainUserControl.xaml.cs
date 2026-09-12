330056381042df22e38cd08a6e7b75289f1adf30
Landon <landon@example.com>
2026-09-12 13:24:32 -0500

    Compact system monitor controls and resize behavior

--- widget-launcher/src/StatsUsageWidget/Widgets/SystemMonitor/Views/MainUserControl.xaml.cs
+++ widget-launcher/src/StatsUsageWidget/Widgets/SystemMonitor/Views/MainUserControl.xaml.cs
@@ -1,26 +1,56 @@
+using System.ComponentModel;
 using System.Windows.Controls;
 using System.Windows;
 using System.Windows.Threading;
+using StatsUsageWidget.Widgets.SystemMonitor.ViewModels;
 
 namespace StatsUsageWidget.Widgets.SystemMonitor.Views;
 
 public partial class MainUserControl : UserControl
 {
+    private const double HostWidthAllowance = 24;
+    private const double HostHeightAllowance = 24;
+    private SystemMonitorViewModel? _viewModel;
+
     public MainUserControl()
     {
         InitializeComponent();
         Loaded += OnLoaded;
         SizeChanged += OnSizeChanged;
+        DataContextChanged += OnDataContextChanged;
     }
 
     private void OnLoaded(object sender, RoutedEventArgs e)
     {
+        AttachViewModel(DataContext as SystemMonitorViewModel);
         Dispatcher.BeginInvoke(ApplyHostWindowSize, DispatcherPriority.Loaded);
+    }
+
+    private void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+    {
+        AttachViewModel(e.NewValue as SystemMonitorViewModel);
     }
 
     private void OnSizeChanged(object sender, SizeChangedEventArgs e)
     {
         Dispatcher.BeginInvoke(ApplyHostWindowSize, DispatcherPriority.Loaded);
+    }
+
+    private void AttachViewModel(SystemMonitorViewModel? viewModel)
+    {
+        if (ReferenceEquals(_viewModel, viewModel)) return;
+        if (_viewModel is not null) _viewModel.PropertyChanged -= OnViewModelPropertyChanged;
+        _viewModel = viewModel;
+        if (_viewModel is not null) _viewModel.PropertyChanged += OnViewModelPropertyChanged;
+    }
+
+    private void OnViewModelPropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName is nameof(SystemMonitorViewModel.WidgetWidth) or
+            nameof(SystemMonitorViewModel.WidgetHeight))
+        {
+            Dispatcher.BeginInvoke(ApplyHostWindowSize, DispatcherPriority.Loaded);
+        }
     }
 
     private void ApplyHostWindowSize()
@@ -36,8 +66,8 @@ public partial class MainUserControl : UserControl
 
         try
         {
-            hostWindow.Width = Width + 104;
-            hostWindow.Height = Height + 24;
+            hostWindow.Width = Width + HostWidthAllowance;
+            hostWindow.Height = Height + HostHeightAllowance;
         }
         catch (InvalidOperationException)
         {
