163b23bdc5a941e749097f9d4f910f6427e231f8
Landon <landon@example.com>
2026-09-12 08:22:48 -0500

    Tighten transparent system monitor layout

--- widget-launcher/src/StatsUsageWidget/Widgets/SystemMonitor/Views/MainUserControl.xaml.cs
+++ widget-launcher/src/StatsUsageWidget/Widgets/SystemMonitor/Views/MainUserControl.xaml.cs
@@ -1,4 +1,6 @@
 using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Threading;
 
 namespace StatsUsageWidget.Widgets.SystemMonitor.Views;
 
@@ -7,5 +9,22 @@ public partial class MainUserControl : UserControl
     public MainUserControl()
     {
         InitializeComponent();
+        Loaded += OnLoaded;
+    }
+
+    private void OnLoaded(object sender, RoutedEventArgs e)
+    {
+        Dispatcher.BeginInvoke(ApplyHostWindowSize, DispatcherPriority.Loaded);
+    }
+
+    private void ApplyHostWindowSize()
+    {
+        var hostWindow = Window.GetWindow(this);
+        if (hostWindow is null ||
+            string.Equals(hostWindow.Title, "Widget Launcher", StringComparison.OrdinalIgnoreCase))
+            return;
+
+        hostWindow.Width = Width + 104;
+        hostWindow.Height = Height + 24;
     }
 }
